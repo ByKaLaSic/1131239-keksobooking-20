2ee0e6e59e1fa793dc7414d7853cd167505a2aa8
Максим <makszavr.1234554321@yandex.ru>
2020-06-26 02:30:57 +0300

    Сделал 5

--- js/card.js
+++ js/card.js
@@ -17,55 +17,57 @@
     }
   };
 
+  var getCreateCard = function (publicity) {
+    var cardElement = cardTemplate.cloneNode(true);
+    var blcokPhotos = cardElement.querySelector('.popup__photos');
+    var popupFeaturesList = cardElement.querySelector('.popup__features');
+    var popupClose = cardElement.querySelector('.popup__close');
+
+    var makeTextElement = function (className, text) {
+      cardElement.querySelector(className).textContent = text;
+    };
+
+    makeTextElement('.popup__title', publicity.offer.title);
+    makeTextElement('.popup__text--address', publicity.offer.address);
+    makeTextElement('.popup__text--price', publicity.offer.price + '₽/ночь');
+    makeTextElement('.popup__type', window.main.HousesTypes[publicity.offer.type]);
+    makeTextElement('.popup__text--capacity', publicity.offer.rooms + ' комнаты для ' + publicity.offer.guests + ' гостей');
+    makeTextElement('.popup__text--time', 'Заезд поесле ' + publicity.offer.checkin + ', выезд до ' + publicity.offer.checkout);
+    makeTextElement('.popup__description', publicity.offer.description);
+    cardElement.querySelector('.popup__avatar').src = publicity.author.avatar;
+
+    for (var i = 0; i < publicity.offer.features.length; i++) {
+      var featureElement = document.createElement('li');
+      featureElement.classList.add('popup__feature');
+      featureElement.classList.add('popup__feature--' + publicity.offer.features[i]);
+      popupFeaturesList.appendChild(featureElement);
+    }
+
+    for (i = 0; i < publicity.offer.photos.length; i++) {
+      if (i === 0) {
+        var photo = blcokPhotos.querySelector('.popup__photo');
+        photo.src = publicity.offer.photos[0];
+      } else {
+        var newPhoto = photo.cloneNode();
+        newPhoto.src = publicity.offer.photos[i];
+        blcokPhotos.appendChild(newPhoto);
+      }
+    }
+
+    cards.push(cardElement);
+
+    popupClose.addEventListener('click', function () {
+      closePopup();
+    });
+
+    return cardElement;
+  };
+
   window.card = {
-    getCreateCard: function (publicity) {
-      var cardElement = cardTemplate.cloneNode(true);
-      var blcokPhotos = cardElement.querySelector('.popup__photos');
-      var popupFeaturesList = cardElement.querySelector('.popup__features');
-      var popupClose = cardElement.querySelector('.popup__close');
-
-      var makeTextElement = function (className, text) {
-        cardElement.querySelector(className).textContent = text;
-      };
-
-      makeTextElement('.popup__title', publicity.offer.title);
-      makeTextElement('.popup__text--address', publicity.offer.address);
-      makeTextElement('.popup__text--price', publicity.offer.price + '₽/ночь');
-      makeTextElement('.popup__type', window.main.HousesTypes[publicity.offer.type]);
-      makeTextElement('.popup__text--capacity', publicity.offer.rooms + ' комнаты для ' + publicity.offer.guests + ' гостей');
-      makeTextElement('.popup__text--time', 'Заезд поесле ' + publicity.offer.checkin + ', выезд до ' + publicity.offer.checkout);
-      makeTextElement('.popup__description', publicity.offer.description);
-      cardElement.querySelector('.popup__avatar').src = publicity.author.avatar;
-
-      for (var i = 0; i < publicity.offer.features.length; i++) {
-        var featureElement = document.createElement('li');
-        featureElement.classList.add('popup__feature');
-        featureElement.classList.add('popup__feature--' + publicity.offer.features[i]);
-        popupFeaturesList.appendChild(featureElement);
-      }
-
-      for (i = 0; i < publicity.offer.photos.length; i++) {
-        if (i === 0) {
-          var photo = blcokPhotos.querySelector('.popup__photo');
-          photo.src = publicity.offer.photos[0];
-        } else {
-          var newPhoto = photo.cloneNode();
-          newPhoto.src = publicity.offer.photos[i];
-          blcokPhotos.appendChild(newPhoto);
-        }
-      }
-
-      cards.push(cardElement);
-
-      popupClose.addEventListener('click', function () {
-        closePopup();
-      });
-
-      return cardElement;
-    },
     cardFragment: cardFragment,
     closePopup: closePopup,
     onPopupEscPress: onPopupEscPress,
-    cards: cards
+    cards: cards,
+    getCreateCard: getCreateCard
   };
 })();
--- js/pin.js
+++ js/pin.js
@@ -7,30 +7,31 @@
   var adFilter = document.querySelector('.map__filters-container');
   var map = document.querySelector('.map');
   var pinFragment = document.createDocumentFragment();
+  var getCreatePin = function (publicity, NumberArr) {
+
+    var pinElement = pinTemplate.cloneNode(true);
+    pinElement.style.left = publicity.location.x - PIN_WIDTH / 2 + 'px';
+    pinElement.style.top = publicity.location.y - PIN_HEIGHT + 'px';
+    pinElement.querySelector('img').src = publicity.author.avatar;
+    pinElement.querySelector('img').alt = publicity.offer.title;
+
+    var openPopup = function () {
+      if (document.querySelector('.map__card')) {
+        document.querySelector('.map__card').remove();
+      }
+      map.insertBefore(window.card.cards[NumberArr], adFilter);
+      document.addEventListener('keydown', window.card.onPopupEscPress);
+    };
+
+    pinElement.addEventListener('click', function () {
+      openPopup();
+    });
+
+    return pinElement;
+  };
 
   window.pin = {
-    getCreatePin: function (publicity, NumberArr) {
-
-      var pinElement = pinTemplate.cloneNode(true);
-      pinElement.style.left = publicity.location.x - PIN_WIDTH / 2 + 'px';
-      pinElement.style.top = publicity.location.y - PIN_HEIGHT + 'px';
-      pinElement.querySelector('img').src = publicity.author.avatar;
-      pinElement.querySelector('img').alt = publicity.offer.title;
-
-      var openPopup = function () {
-        if (document.querySelector('.map__card')) {
-          document.querySelector('.map__card').remove();
-        }
-        map.insertBefore(window.card.cards[NumberArr], adFilter);
-        document.addEventListener('keydown', window.card.onPopupEscPress);
-      };
-
-      pinElement.addEventListener('click', function () {
-        openPopup();
-      });
-
-      return pinElement;
-    },
-    pinFragment: pinFragment
+    pinFragment: pinFragment,
+    getCreatePin: getCreatePin
   };
 })();
