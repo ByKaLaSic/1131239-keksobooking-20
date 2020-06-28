60b8040255f288fa7d27d5db9a52dcba3d8691f9
Максим <makszavr.1234554321@yandex.ru>
2020-06-29 01:41:10 +0300

    Сделал один запрос

--- js/map.js
+++ js/map.js
@@ -3,17 +3,30 @@
 (function () {
   var map = document.querySelector('.map');
   var mainPin = document.querySelector('.map__pin--main');
-  mainPin.addEventListener('keydown', function (evt) {
-    if (window.utils.isEnterPressed(evt)) {
-      window.load(window.main.successHandler, window.main.errorHandler);
-    }
-  });
 
-  mainPin.addEventListener('mousedown', function (evt) {
-    if (window.utils.isLeftButtonPressed(evt)) {
-      window.load(window.main.successHandler, window.main.errorHandler);
+  var onMainPinPress = function () {
+    mainPin.removeEventListener('keydown', onEnterPress);
+    mainPin.removeEventListener('mousedown', onLeftButtonMousePress);
+    window.load(window.main.successHandler, window.main.errorHandler);
+  };
+
+  var onEnterPress = function (evt) {
+    if (window.utils.isEnterPressed(evt)) {
+      evt.preventDefault();
+      onMainPinPress();
     }
-  });
+  };
+
+  var onLeftButtonMousePress = function (evt) {
+    if (window.utils.isLeftButtonPressed(evt)) {
+      evt.preventDefault();
+      onMainPinPress();
+    }
+  };
+
+  mainPin.addEventListener('keydown', onEnterPress);
+  mainPin.addEventListener('mousedown', onLeftButtonMousePress);
+
   window.map = {
     mainPin: mainPin,
     map: map
