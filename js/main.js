4912e6a1dd9b453f824a99d1d5f16075855ac81f
Максим <makszavr.1234554321@yandex.ru>
2020-06-28 19:18:41 +0300

    Промежуточный результат

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@
     house: 'Дом'
   };
 
+  var ads;
+
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
     node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
@@ -21,26 +23,29 @@
   };
 
   var successHandler = function (arrAds) {
-    var ads = arrAds;
+    var MAX_SIMILAR_PIN_COUNT = 5;
+    ads = arrAds;
 
     (function () {
-      for (var i = 0; i < ads.length; i++) {
+      for (var i = 0; i < MAX_SIMILAR_PIN_COUNT; i++) {
         window.pin.pinFragment.appendChild(window.pin.getCreatePin(ads[i], i));
       }
     })();
 
     (function () {
-      for (var j = 0; j < ads.length; j++) {
+      for (var j = 0; j < MAX_SIMILAR_PIN_COUNT; j++) {
         window.card.cardFragment.appendChild(window.card.getCreateCard(ads[j]));
       }
     })();
 
+    window.main.ads = ads;
     window.form.activeState();
   };
 
   window.main = {
     HousesTypes: HousesTypes,
     errorHandler: errorHandler,
-    successHandler: successHandler
+    successHandler: successHandler,
+    ads: ads
   };
 })();
--- js/render.js
+++ js/render.js
@@ -4,7 +4,34 @@
   var form = document.querySelector('.map__filters');
   var type = form.querySelector('#housing-type');
 
+  var getRank = function (pin) {
+    var rank = 0;
+
+    if (pin.offer.type === type.value) {
+      rank += 1;
+    }
+
+    return rank;
+  };
+
+  // var namesComparator = function (left, right) {
+  //   if (left > right) {
+  //     return 1;
+  //   } else if (left < right) {
+  //     return -1;
+  //   } else {
+  //     return 0;
+  //   }
+  // };
+
   form.addEventListener('change', function () {
-    console.log(type);
+    var s = window.main.ads.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        // rankDiff = namesComparator(left.name, right.name);
+        console.log('Говно');
+      }
+    });
+    console.log(s);
   });
 }());
