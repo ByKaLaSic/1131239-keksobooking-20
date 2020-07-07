1cf25afe596e769e2d749294ef5aec86cfe2d4b7
Максим <makszavr.1234554321@yandex.ru>
2020-07-07 19:56:00 +0300

    Сделал первое фото

--- js/avatar.js
+++ js/avatar.js
@@ -1,15 +1,18 @@
 'use strict';
 
 (function () {
-  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var FILE_TYPES = ['jpeg', 'png'];
 
   var form = document.querySelector('.ad-form');
-  var asd = form.querySelectorAll('input[type=file]');
-  var fileChooser = document.querySelector('.upload input[type=file]');
-  var preview = document.querySelector('.setup-user-pic');
+  var fileUserPhoto = form.querySelector('.ad-form-header__input');
+  var fileApartmentPhoto = form.querySelector('.ad-form__input');
+  // var fileChooser = document.querySelector('.upload input[type=file]');
+  var userPreview = form.querySelector('.ad-form-header__preview');
+  var apartmentPreview = form.querySelector('.ad-form__photo');
+  // var preview = document.querySelector('.setup-user-pic');
 
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
+  fileUserPhoto.addEventListener('change', function () {
+    var file = fileUserPhoto.files[0];
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
@@ -20,7 +23,26 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        preview.src = reader.result;
+        userPreview.querySelector('img').src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  fileApartmentPhoto.addEventListener('change', function () {
+    var file = fileApartmentPhoto.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        apartmentPreview.src = reader.result;
       });
 
       reader.readAsDataURL(file);
