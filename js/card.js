6a29872ea797d78ed28169d8f90a613e28b69db5
Максим <makszavr.1234554321@yandex.ru>
2020-07-14 02:50:50 +0300

    Исправил баг

--- js/card.js
+++ js/card.js
@@ -8,7 +8,9 @@
   var closePopup = function () {
     document.querySelector('.map__card').remove();
     document.removeEventListener('keydown', onPopupEscPress);
-    document.querySelector('.map__pin.map__pin--active').classList.remove('map__pin--active');
+    if (document.querySelector('.map__pin.map__pin--active')) {
+      document.querySelector('.map__pin.map__pin--active').classList.remove('map__pin--active');
+    }
   };
 
   var onPopupEscPress = function (evt) {
